e0bce618fc78190984b8a825334d8c210607cef2
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-12-04 22:25:24 +0600

    recap update with style like object

--- src/App.js
+++ src/App.js
@@ -4,9 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h2>Hi I am Mahbub Hasan</h2>
+      <h2>Recap Country</h2>
+      <Country name={'Noakhali'} speciality={'bivhag'}></Country>
+      <Country name={'katabon'} speciality={'jaiga nai'}></Country>
+      <Country name={'shundorbon'} speciality={'lage dom'}></Country>
     </div>
   );
 }
+
+
+const styleSence={
+  backgroundColor: 'red',
+  margin: '20px',
+  borderRadius: '20px',
+  padding: '20px'
+}
+
+function Country(props) {
+  return (
+    <div style={styleSence}>
+      <h2>Country Name: {props.name}</h2>
+      <p>Speciality: {props.speciality}</p>
+    </div>
+  )
+}
+
+
+
+
 
 export default App;
